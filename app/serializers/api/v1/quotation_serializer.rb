13b5a4fa1e69a32bf9854d882d6b84433c2f6060
Sergiy.D <Golfersds@gmail.com>
2019-01-09 15:07:09 +0200

    Add Author Full-name to API serializer because ReactJS do not get Author Object[WIP - need think about it -WHY it happens]

--- app/serializers/api/v1/quotation_serializer.rb
+++ app/serializers/api/v1/quotation_serializer.rb
@@ -1,9 +1,13 @@
 module Api
   module V1
     class QuotationSerializer < Api::ApplicationSerializer
-      attributes :id, :body
+      attributes :id, :body, :author_full_name
       has_many :tags, include: true
       has_one :author
+
+      def author_full_name
+        object&.author&.full_name
+      end
     end
   end
 end
